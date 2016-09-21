1e28e678083c213e96535dd56a90b9ef914daf7f
Hanks10100 <zhanghan.me@gmail.com>
2016-09-21 19:40:16 +0800

    check Android env before running

--- src/run/runAndroid.js
+++ src/run/runAndroid.js
@@ -1,10 +1,20 @@
 const chalk = require('chalk')
+const utils = require('../utils')
 
 /**
- * Run Android app
+ * Starts Android app on a connected emulator or device
  * @param {Object} options
  */
 function runAndroid(options) {
+  if (!utils.checkAndroid(process.cwd())) {
+    console.log()
+    console.log(chalk.red('  Android project not found !'))
+    console.log()
+    console.log(`  You should run ${chalk.blue('weexpack init')} first`)
+    return
+  }
+
+  console.log()
   console.log(` => ${chalk.blue.bold('Will start Android app')}`)
 }
 
--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,3 +1,14 @@
+const fs = require('fs')
+const path = require('path')
+
+/**
+ * Verifies this is an Android project
+ * @param {String} root directory path
+ */
+function checkAndroid(cwd) {
+  return fs.existsSync(path.join(cwd, 'android/bin/gradlew'))
+}
 
 module.exports = {
+  checkAndroid,
 }
