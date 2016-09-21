5bd5ffb2c25353df7cc77ebd5e35708040e503d1
Hanks10100 <zhanghan.me@gmail.com>
2016-09-21 19:04:47 +0800

    enable to run specific app

---
+++ src/run/runAndroid.js
@@ -0,0 +1,11 @@
+const chalk = require('chalk')
+
+/**
+ * Run Android app
+ * @param {Object} options
+ */
+function runAndroid(options) {
+  console.log(` => ${chalk.blue.bold('Will start Android app')}`)
+}
+
+module.exports = runAndroid
---
+++ src/run/runIOS.js
@@ -0,0 +1,11 @@
+const chalk = require('chalk')
+
+/**
+ * Run iOS app
+ * @param {Object} options
+ */
+function runIOS(options) {
+  console.log(` => ${chalk.blue.bold('Will start iOS app')}`)
+}
+
+module.exports = runIOS
