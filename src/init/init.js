6c0d3b49231786328da7bfb71159888ee696b18a
Hanks10100 <zhanghan.me@gmail.com>
2016-09-21 11:21:16 +0800

    enable to use custom config file for init command

--- src/init/init.js
+++ src/init/init.js
@@ -1,9 +1,11 @@
 
 /**
  * Initialize a standard weex project
- * @param [string] project name
+ * @param {String} project name
+ * @param {String} config file path
  */
-function init(projectName) {
+function init(projectName, configFile) {
+  console.log(`Initialize a new Weex app (${projectName})`)
 }
 
 module.exports = init
