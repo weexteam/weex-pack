605bbb75d34682e2f4f74758d23fc328c6c68b83
Hanks10100 <zhanghan.me@gmail.com>
2016-09-21 15:07:23 +0800

    create android and ios generators

--- generator/index.js
+++ generator/index.js
@@ -1,4 +1,3 @@
-const fs = require('fs')
 const path = require('path')
 const yeoman = require('yeoman-generator')
 
@@ -6,9 +5,37 @@ module.exports = yeoman.Base.extend({
 
   constructor: function () {
     yeoman.Base.apply(this, arguments)
+
+    // compose with Android and iOS generator
+    const args = { args: arguments[0], options: this.options }
+    this.composeWith('weex:android', args, {
+      local: require.resolve(path.resolve(__dirname, 'android'))
+    })
+    this.composeWith('weex:ios', args, {
+      local: require.resolve(path.resolve(__dirname, 'ios'))
+    })
   },
 
-  output: function () {
-    console.log(' => run yeoman generator')
+  configuring: function() {
+  },
+
+  writing: function() {
+    this.fs.copy(this.templatePath('**/*'), this.destinationPath())
+
+    // const copyTo = (src, dist) => {
+    //   if (src && dist) {
+    //     this.fs.copy(this.templatePath(src), this.destinationPath(dist))
+    //   }
+    // }
+
+    // copyTo('_gitignore', '.gitignore')
+    // copyTo('android', 'android')
+    // copyTo('_buckconfig', '.buckconfig')
+  },
+
+  install: function() {
+  },
+
+  end: function() {
   }
 })
