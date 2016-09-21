2728a24972c2bcd536b5ab0f65e2eed0055438cc
Hanks10100 <zhanghan.me@gmail.com>
2016-09-22 00:34:36 +0800

    enable to start web service

---
+++ src/run/web.js
@@ -0,0 +1,12 @@
+const chalk = require('chalk')
+
+/**
+ * Start web service
+ * @param {Object} options
+ */
+function runWeb(options) {
+  console.log()
+  console.log(` => ${chalk.blue.bold('Will start web service')}`)
+}
+
+module.exports = runWeb
