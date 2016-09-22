0c9bd745d528d563073b1dda05318da502ad73a7
fkysly <fkysly@gmail.com>
2016-09-22 16:20:17 +0800

    mv ios to generator

---
+++ generator/ios/templates/WeexSample/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  WeexSample
+//
+//  Created by zifan.zx on 6/12/16.
+//  Copyright © 2016. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (nonatomic, strong) NSURL *url;
+@end
+
