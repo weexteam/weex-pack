d96280722cd465dd6dddd6405ff596678b9ba9d0
Hanks10100 <zhanghan.me@gmail.com>
2016-09-22 14:29:45 +0800

    add findXcodeProject in utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path')
 const output = require('./output')
 const validator = require('./validator')
 
@@ -16,6 +17,35 @@ const utils = {
       })
       fs.writeFileSync(dest, content)
     }
+  },
+
+  /**
+   * Find xcode project in particular directory
+   * @param {String} directory
+   * @return {Object} project info
+   */
+  findXcodeProject(dir) {
+    const files = fs.readdirSync(dir)
+    const sortedFiles = files.sort()
+    for (let i = sortedFiles.length - 1; i >= 0; i--) {
+      const fileName = files[i]
+      const ext = path.extname(fileName)
+
+      if (ext === '.xcworkspace') {
+        return {
+          name: fileName,
+          isWorkspace: true,
+        }
+      }
+      if (ext === '.xcodeproj') {
+        return {
+          name: fileName,
+          isWorkspace: false,
+        }
+      }
+    }
+
+    return null
   },
 
 }
