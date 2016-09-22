7f45c68299e89c2eb9d25922752324feb2f23604
Hanks10100 <zhanghan.me@gmail.com>
2016-09-22 09:27:48 +0800

    add platform detection in utils

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -9,6 +9,30 @@ function checkAndroid(cwd) {
   return fs.existsSync(path.join(cwd, 'android/bin/gradlew'))
 }
 
+/**
+ * Check if current cli is running on Windows platform
+ */
+function isOnWindows() {
+  return /^win/.test(process.platform)
+}
+
+/**
+ * Check if current cli is running on OSX platform
+ */
+function isOnMac() {
+  return process.platform === 'darwin'
+}
+
+/**
+ * Check if current cli is running on Linux platform
+ */
+function isOnLinux() {
+  return process.platform === 'linux'
+}
+
 module.exports = {
   checkAndroid,
+  isOnWindows,
+  isOnMac,
+  isOnLinux,
 }
