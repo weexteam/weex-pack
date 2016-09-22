be05c9e3e32242592688bcd1b844182aeb596d99
Hanks10100 <zhanghan.me@gmail.com>
2016-09-22 14:30:55 +0800

    implement basic api for run iOS app

--- src/run/iOS.js
+++ src/run/iOS.js
@@ -1,11 +1,55 @@
+const path = require('path')
 const chalk = require('chalk')
+const child_process = require('child_process')
+const utils = require('../utils')
 
 /**
  * Run iOS app
  * @param {Object} options
  */
 function runIOS(options) {
-  console.log(` => ${chalk.blue.bold('Will start iOS app')}`)
+  const rootPath = process.cwd()
+
+  if (!utils.checkIOS(rootPath)) {
+    console.log()
+    console.log(chalk.red('  iOS project not found !'))
+    console.log()
+    console.log(`  You should run ${chalk.blue('weexpack init')} first`)
+    return
+  }
+
+  // change working directory to ios
+  process.chdir(path.join(rootPath, 'ios'))
+
+  const xcodeProject = utils.findXcodeProject(process.cwd())
+
+  if (xcodeProject) {
+    console.log()
+    console.log(` => ${chalk.blue.bold('Will start iOS app')}`)
+
+  } else {
+    console.log()
+    console.log(`  ${chalk.red.bold('Could not find Xcode project files in ios folder')}`)
+    console.log()
+    console.log(`  Please make sure you have installed iOS Develop Environment and CocoaPods`)
+    console.log(`  See ${chalk.cyan('http://alibaba.github.io/weex/doc/advanced/integrate-to-ios.html')}`)
+  }
+}
+
+/**
+ * Build the iOS app
+ * @param {Object} xcode project
+ * @param {Object} options
+ */
+function buildApp(xcodeProject, options) {
+}
+
+/**
+ * Run the iOS app on emulator or device
+ * @param {Object} xcode project
+ * @param {Object} options
+ */
+function runApp(xcodeProject, options) {
 }
 
 module.exports = runIOS
