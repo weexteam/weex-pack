76f45fe187e1562df66a77465a81a44fa15dbf50
yangshengtao <shengtao.yst@alibaba-inc.com>
2016-11-01 11:50:55 +0800

    add devtool link when running iOS for emulator

--- generator/ios/templates/playground/WeexDemo/DemoDefine.h
+++ generator/ios/templates/playground/WeexDemo/DemoDefine.h
@@ -16,6 +16,8 @@
     #define DEMO_HOST CURRENT_IP
 #endif
 
+#define WXDEVTOOL_IP @"127.0.0.1:8088"
+
 #define DEMO_URL(path) [NSString stringWithFormat:@"http://%@:12580/%s", DEMO_HOST, #path]
 
 #define HOME_URL [NSString stringWithFormat:@"http://%@:8080/dist/index.js", DEMO_HOST]
@@ -23,6 +25,8 @@
 #define BUNDLE_URL [NSString stringWithFormat:@"file://%@/bundlejs/index.js",[NSBundle mainBundle].bundlePath]
 
 #define UITEST_HOME_URL @"http://test?_wx_tpl=http://localhost:12580/test/build/TC__Home.js"
+
+#define WXDEVTOOL_URL [NSString stringWithFormat:@"ws://%@/debugProxy/native",WXDEVTOOL_IP]
 
 #define QRSCAN  @"com.taobao.WeexDemo.scan"
 #define WEEX_COLOR [UIColor colorWithRed:0.27 green:0.71 blue:0.94 alpha:1]
