fd4ae76d6e173d98b252b0cb8d66f129e4793506
gaosong <rex.gs@alibaba-inc.com>
2016-12-15 11:58:54 +0800

    add publish

--- src/publish/market.js
+++ src/publish/market.js
@@ -1,52 +1,63 @@
-exports.publish=function(package,readme){
+const Url = require('url');
+const Http = require('http');
+const chalk=require('chalk');
+exports.publish = function (name, readme) {
+  return new Promise(function(resolve,reject){
+    post('http://10.218.136.234//json/sync/sync.json?name='+name).then(function(data){
+      console.log(chalk.yellow('plugin ['+name+']publish success!'));
+      resolve()
+    }).catch(function(){
+      console.log(chalk.red('market sync failed!'));
+      reject();
+    })
+  })
 
-}
-var post = function(url, data) {
-  var urlObj = Url.parse(url);
-  if(data) {
-    data = new Buffer(JSON.stringify(data));
-    var headers = {
-      "Content-Type"   : 'application/json;charset=UTF-8',
-      "Content-Length" : data.length
-    }
-  }
-  else {
-    headers = {
-      "Content-Length" : 0
-    }
-  }
+};
+var post = function (url, data) {
+  return new Promise(function (resolve, reject) {
 
-  var p = new Promise();
-  var req = Http.request({
-    host    : urlObj.hostname,
-    method  : 'post',
-    path    : urlObj.path,
-    port    : urlObj.port || 80,
-    headers : headers
-  }, function(res) {
-    var body = '';
-    res.on('data', function(chunk) {
-      body += chunk.toString();
-    });
-    res.on('end', function() {
-      if(res.statusCode == 200) {
-        p.resolve(body);
+
+    var urlObj = Url.parse(url);
+    if (data) {
+      data = new Buffer(JSON.stringify(data));
+      var headers = {
+        "Content-Type": 'application/json;charset=UTF-8',
+        "Content-Length": data.length
       }
-      else {
-        if(res.statusCode==500){
-          console.warn('500 Error:',url,data);
+    }
+    else {
+      headers = {
+        "Content-Length": 0
+      }
+    }
+
+    var req = Http.request({
+      host: urlObj.hostname,
+      method: 'post',
+      path: urlObj.path,
+      port: urlObj.port || 80,
+      headers: headers
+    }, function (res) {
+      var body = '';
+      res.on('data', function (chunk) {
+        body += chunk.toString();
+      });
+      res.on('end', function () {
+        if (res.statusCode == 200) {
+          resolve(body);
         }
-        p.reject(res.statusCode);
-      }
+        else {
+          reject(res.statusCode);
+        }
+      });
     });
-  });
 
-  req.on('error', function(err) {
-    var e = new Error('Connect Error for request for ' + url);
-    e.name = 'Http Request Error';
-    p.reject(e);
-  });
-  if(data != null)req.write(data);
-  req.end();
-  return p;
+    req.on('error', function (err) {
+      var e = new Error('Connect Error for request for ' + url);
+      e.name = 'Http Request Error';
+      reject(e);
+    });
+    if (data != null)req.write(data);
+    req.end();
+  })
 };
--- src/publish/publish.js
+++ src/publish/publish.js
@@ -1,7 +1,38 @@
 /**
  * Created by godsong on 16/11/29.
  */
-
-exports.publish=function(){
-
+const PluginInfo=require('cordova-common').PluginInfo;
+const Chalk = require('chalk');
+const Npm=require('../utils/npm');
+const Fs=require('fs');
+const Market=require('./market');
+module.exports=function(ali){
+  let plugin;
+  try {
+    plugin = new PluginInfo('./');
+  }catch(e){
+    console.log();
+    console.log(Chalk.red('  weex plugin project not found !'));
+    console.log();
+    console.log(`  You should run ${Chalk.blue('weexpack plugin create')} first`);
+    console.log();
+    process.exit();
+  }
+  var package={};
+  package.name=Npm.prefix+plugin.id;
+  package.version=plugin.version;
+  package.platform=plugin.getPlatformsArray();
+  package.description=plugin.description;
+  package.keywords=plugin.keywords;
+  package.license=plugin.license;
+  package.description=plugin.description;
+  if(ali){
+    package.publishConfig={
+      registry: 'http://registry.npm.alibaba-inc.com'
+    }
+  }
+  Fs.writeFileSync('./package.json',JSON.stringify(package,null,4));
+  Npm.publish(ali,true).then(function(){
+    Market.publish(package.name);
+  })
 };
