ac649e8ab67ccaa0c68ea23be08e2ba5bec83603
gaosong <rex.gs@alibaba-inc.com>
2016-12-23 16:52:37 +0800

    add  new publish

--- src/publish/market.js
+++ src/publish/market.js
@@ -1,22 +1,66 @@
 const Url = require('url');
 const Http = require('http');
-const chalk=require('chalk');
-exports.publish = function (name, version) {
-  return new Promise(function(resolve,reject){
-    post('http://10.218.136.234/json/sync/sync.json?name='+name).then(function(data){
-      console.log();
-      console.log(chalk.yellow('plugin ['+name+'@'+version+'] publish success! sync to market maybe need a few minutes.'));
-      console.log(chalk.yellow(`you can visit ${exports.domain} see your plugin. if not exist you can retry ${chalk.blue('weexpack plugin publish')}` ))
-      console.log();
-      resolve()
-    }).catch(function(){
+const chalk = require('chalk');
+const crypto = require('crypto');
+exports.domain = 'http://weex-market.taobao.net';
+exports.publish = function (name, namespace, ali, version) {
+  return new Promise(function (resolve, reject) {
+    var md5 = crypto.createHash('md5');
+    md5.update(`name=${name}_fullname=${namespace + '-' + name}_p=${!!ali}`);
+    let sign = md5.digest('hex');
+    let url = exports.domain + '/json/sync/sync.json?name=' + name + '&namespace=' + namespace + '&fullname=' + namespace + '-' + name + '&p=' + !!ali + '&sign=' +'123';
+    post(url).then(function (res) {
+      if (res.success) {
+        console.log();
+        console.log(chalk.yellow('plugin [' + name + '@' + version + '] publish success! sync to market maybe need a few minutes.'));
+        console.log(chalk.yellow(`you can visit ${exports.domain} see your plugin. if not exist you can retry ${chalk.blue('weexpack plugin publish')}`))
+        console.log();
+        resolve()
+      }
+      else if (res.data.code == 10004) {
+        console.log(chalk.red(`Market sync rejected! Namespace unmatched!`));
+      }
+    }).catch(function () {
       console.log(chalk.red(`Market sync failed! Please retry ${chalk.blue('weexpack plugin publish')}`));
       reject();
     })
   })
 
 };
-exports.domain='http://weex-market.taobao.net/';
+
+exports.apply = function (name, p) {
+  return new Promise((resolve, reject)=> {
+    post(exports.domain + '/json/sync/apply.json?name=' + name + '&p=' + !!p).then(function (res) {
+      if (res.success) {
+        resolve(res.data);
+      }
+      else {
+        throw new Error();
+      }
+
+    }).catch(function (e) {
+      console.log();
+      console.log(chalk.red(`Market apply failed! Please retry ${chalk.blue('weexpack plugin publish')}`));
+      console.log();
+      reject();
+    })
+  })
+};
+exports.info = function (name) {
+  return new Promise((resolve, reject)=> {
+    post(exports.domain + '/json/sync/info.json?name=' + name).then(function (res) {
+      if (res.success) {
+        resolve(res.data)
+      }
+      else {
+        reject(res);
+      }
+
+    }, function (e) {
+      reject(e);
+    })
+  })
+}
 var post = function (url, data) {
   return new Promise(function (resolve, reject) {
     var urlObj = Url.parse(url);
@@ -46,7 +90,11 @@ var post = function (url, data) {
       });
       res.on('end', function () {
         if (res.statusCode == 200) {
-          resolve(body);
+          try {
+            resolve(JSON.parse(body));
+          } catch (e) {
+            resolve(body);
+          }
         }
         else {
           reject(res.statusCode);
--- src/publish/publish.js
+++ src/publish/publish.js
@@ -6,52 +6,71 @@ const Chalk = require('chalk');
 const Npm = require('../utils/npm');
 const Fs = require('fs');
 const Market = require('./market');
+const Cache = require('../utils/cache');
 module.exports = function (ali) {
   let plugin;
   try {
     plugin = new PluginInfo('./');
   } catch (e) {
-    console.log();
-    console.log(Chalk.red('  weex plugin project not found !'));
-    console.log();
-    console.log(`  You should run ${Chalk.blue('weexpack plugin create')} first`);
-    console.log();
-    process.exit();
-  }
-  var weexpackCache;
-  if(Fs.existsSync('./.weexpack.cache')){
-    weexpackCache=JSON.parse(Fs.readFileSync('./.weexpack.cache').toString());
-  }
-  else{
-    weexpackCache={latest:'0.0.0'};
-  }
-  var package = {};
-  package.name = Npm.prefix + plugin.id;
-  package.version = plugin.version;
-  package.platform = plugin.getPlatformsArray();
-  package.description = plugin.description;
-  package.keywords = plugin.keywords;
-  package.license = plugin.license;
-  package.description = plugin.description;
-  if (ali) {
-    package.publishConfig = {
-      registry: 'http://registry.npm.alibaba-inc.com'
+    if (e.message.indexOf('Cannot find plugin.xml for plugin') != -1) {
+      console.log();
+      console.log(Chalk.red('  weex plugin project not found !'));
+      console.log();
+      console.log(`  You should run ${Chalk.blue('weexpack plugin create')} first`);
+      console.log();
+      process.exit();
+    }
+    else {
+      console.log();
+      console.log(Chalk.red(e.message));
+      console.log();
     }
   }
-  if (package.version > weexpackCache.latest) {
-    Fs.writeFileSync('./package.json', JSON.stringify(package, null, 4));
-    Npm.publish(ali, true).then(function (success) {
-      if (success) {
-        weexpackCache.latest=package.version;
-        Market.publish(package.name, package.version);
-        Fs.writeFileSync('./.weexpack.cache',JSON.stringify(weexpackCache,null,4));
+  Cache.init();
 
+  if (plugin.version > Cache.get('latestVersion', '0.0.0')) {
+    var package = {};
+    package.version = plugin.version;
+    package.platform = plugin.getPlatformsArray();
+    package.description = plugin.description;
+    package.keywords = plugin.keywords;
+    package.license = plugin.license;
+    package.description = plugin.description;
+    if (ali) {
+      package.publishConfig = {
+        registry: 'http://registry.npm.alibaba-inc.com'
       }
+    }
+    let namespace = Cache.get('namespace');
+    if (!namespace) {
+      Market.apply(plugin.id, ali).then(function (result) {
+        Cache.cache.namespace = result.namespace;
+        package.name = result.fullname;
+        _doPublish(package,plugin.id,result.namespace,ali)
+      },function(){
 
-    })
+      });
+    }
+    else {
+      package.name = namespace + '-' + plugin.id;
+      _doPublish(package, plugin.id,namespace,ali)
+    }
+
   }
-  else {
-    Market.publish(package.name, package.version);
+  else if(Cache.get('namespace')){
+    Market.publish(plugin.id, Cache.get('namespace'),ali,plugin.version).then(function(){
+
+    });
   }
 
 };
+function _doPublish(package, name,namespace,ali) {
+  Fs.writeFileSync('./package.json', JSON.stringify(package, null, 4));
+  Npm.publish(ali, true).then(function (success) {
+    if (success) {
+      Market.publish(name,namespace,ali,package.version);
+      Cache.cache.latestVersion = package.version;
+      Cache.save();
+    }
+  })
+}
