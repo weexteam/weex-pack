9d095f59333e20b8162697310a06283929685ccb
JackPu <kakashjack@gmail.com>
2017-01-03 17:01:02 +0800

    add build plugin webpack build

--- src/build/Web.js
+++ src/build/Web.js
@@ -8,22 +8,56 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const utils = require('../utils')
 const runWeb = require('../run/web');
+let pluginArr = [];
 
 function buildWeb(options) {
-  if(checkOldTemplate()) {
-   // return ;
-  }
-  runWeb();
-  
+    if (checkOldTemplate()) {
+        // return ;
+    }
+    
+    buildPlugin().then(() => {
+       runWeb(); 
+    });
 }
 
-// if use old weexpack please remove some directoies to /platforms 
+function buildPlugin() {
+    // check plugin history
+    let rootPath = process.cwd();
+    if (!fs.existsSync(path.join(rootPath, 'plugins/fetch.json'))) {
+        return;
+    }
+    let plugins = require(path.join(rootPath, 'plugins/fetch.json'));
+    for (let k in plugins) {
+        pluginArr.push(k);
+    }
+    let result = Promise.resolve();
+    pluginArr.forEach((plugin) => {
+        result = result.then(() => {
+            return fs.writeFile(path.join(rootPath, './plugins/' + plugin + '.js'), 'require("' + plugin + '")', function (err) {})
+        });
+    });
+    return result.then(() => {
+        buildSinglePlugin();
+    }).catch((err) => {
+        console.log(err);
+    })
+}
+// if use old weexpack please move some directoies to /platforms 
 function checkOldTemplate() {
-  if (fs.existsSync(path.join('./', 'web'))) {
-    console.log(chalk.red('please remove "web" directory into "platforms"'));
-    console.log('(new version weexpack not support old directoies)');
-    return true;
-  }
-  return false;
+    if (fs.existsSync(path.join('./', 'web'))) {
+        console.log(chalk.red('please remove "web" directory into "platforms"'));
+        console.log('(new version weexpack not support old directoies)');
+        return true;
+    }
+    return false;
+}
+// build single plugin use webpack
+function buildSinglePlugin() {
+    try {
+        utils.buildJS('build_plugin');
+    }
+    catch (e) {
+        console.error(e);
+    }
 }
 module.exports = buildWeb;
