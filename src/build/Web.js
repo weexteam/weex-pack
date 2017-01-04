8379643da7f664ac794cfd7caa5ca70cfa63390b
JackPu <kakashjack@gmail.com>
2017-01-04 10:55:01 +0800

    add plugin js clear

--- src/build/Web.js
+++ src/build/Web.js
@@ -10,55 +10,55 @@ const utils = require('../utils')
 let pluginArr = [];
 
 function buildWeb(options) {
-    /*if (checkOldTemplate()) {
-        // return ;
-    }*/
-    buildPlugin()
+  /*if (checkOldTemplate()) {
+      // return ;
+  }*/
+  buildPlugin()
 }
-
 // if use old weexpack please move some directoies to /platforms 
 function checkOldTemplate() {
-    if (fs.existsSync(path.join('./', 'web'))) {
-        console.log(chalk.red('please remove "web" directory into "platforms"'));
-        console.log('(new version weexpack not support old directoies)');
-        return true;
-    }
-    return false;
+  if (fs.existsSync(path.join('./', 'web'))) {
+    console.log(chalk.red('please remove "web" directory into "platforms"'));
+    console.log('(new version weexpack not support old directoies)');
+    return true;
+  }
+  return false;
 }
 
 function buildPlugin() {
-    let rootPath = process.cwd();
-    if (!fs.existsSync(path.join(rootPath, 'plugins/fetch.json'))) {
-        return;
-    }
-    // check plugin history
-    let plugins = require(path.join(rootPath, 'plugins/fetch.json'));
-    for (let k in plugins) {
-        pluginArr.push(k);
-    }
-    let result = Promise.resolve();
-    pluginArr.forEach((plugin) => {
-        result = result.then(() => {
-            return fs.writeFile(path.join(rootPath, './plugins/' + plugin + '.js'), 'require("' + plugin + '")', function (err) {})
-        });
+  let rootPath = process.cwd();
+  if (!fs.existsSync(path.join(rootPath, 'plugins/fetch.json'))) {
+    return;
+  }
+  // check plugin history
+  let plugins = require(path.join(rootPath, 'plugins/fetch.json'));
+  for (let k in plugins) {
+    pluginArr.push(k);
+  }
+  let result = Promise.resolve();
+  pluginArr.forEach((plugin) => {
+    result = result.then(() => {
+      return fs.writeFile(path.join(rootPath, './plugins/' + plugin + '.js'), 'require("' + plugin + '")', function (err) {})
     });
-    return result.then(() => {
-        buildSinglePlugin();
-    }).catch((err) => {
-        console.log(err);
-    })
+  });
+  return result.then(() => {
+    buildSinglePlugin();
+  }).catch((err) => {
+    console.log(err);
+  })
 }
-
 // build single plugin use webpack
 function buildSinglePlugin() {
-    
-    try {
-        utils.buildJS('build_plugin').then(() => {
-            utils.exec('npm run build',true)
-        });
-    }
-    catch (e) {
-        console.error(e);
-    }
+  try {
+    utils.buildJS('build_plugin').then(() => {
+      utils.exec('npm run build', true);
+      pluginArr.forEach((plugin) => {
+        fs.unlink(path.join(rootPath, './plugins/' + plugin + '.js'));
+      });
+    });
+  }
+  catch (e) {
+    console.error(e);
+  }
 }
 module.exports = buildWeb;
