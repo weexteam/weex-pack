1dff9aa53ef583fe5e9ae7cb2c7161e61019bc65
卜道 <705040424@qq.com>
2017-01-09 17:27:18 +0800

    add a util script for test

---
+++ alpha/android_test_local.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#plugin_path="/Users/budao/weex-plugins/weex-plugin-chart"
+while getopts "d:h" arg
+do
+    case $arg in
+        d)
+        echo "plugin path is :$OPTARG"
+        plugin_path=$OPTARG
+        ;;
+        h)
+        echo "Usage: ship [-d plugin_path]"
+        echo "-d plugin dir"
+        exit 1
+        ;;
+        ?)
+        echo "unknown argument"
+        exit 1
+        ;;
+    esac
+done
+
+rm -rf MyApp
+
+weexpack create MyApp
+cd MyApp && cnpm install
+
+weexpack platform add android -d
+
+weexpack plugin add "$plugin_path" -d
+cp -ri "$plugin_path/examples/" src
+
+adb devices
+weexpack run android
