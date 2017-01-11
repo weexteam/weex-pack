24113775688175799ff43e8acc95004b4feb8ca7
JackPu <kakashjack@gmail.com>
2017-01-11 11:03:33 +0800

    to 2.0

--- generator/index.js
+++ generator/index.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const yeoman = require('yeoman-generator')
 const utils = require('../src/utils')
+const chalk = require('chalk');
 
 const dependencies = [
   'weex-html5',
@@ -41,7 +42,7 @@ module.exports = yeoman.Base.extend({
 
     copy('README.md')
     copy('webpack.config.js')
-    copy('src/index.we')
+    copy('src/index.vue');
     copy('web/index.html')
     copy('android.config.json')
     copy('ios.config.json')
@@ -55,7 +56,12 @@ module.exports = yeoman.Base.extend({
         // replace project name
         '\\"name\\"\\:\\s*\\"\\w+\\"' : `"name": "${this.options.projectName}"`,
       }
-    )
+     
+    );
+    console.log('you could run "cd ' + this.options.projectName + ' && npm install"');
+    console.log('then you could run the commands:');
+    console.log('npm run build : ' + chalk.grey('build your project'));
+    console.log('npm run serve : ' + chalk.grey('start a local server'));
   },
 
   install: function() {
