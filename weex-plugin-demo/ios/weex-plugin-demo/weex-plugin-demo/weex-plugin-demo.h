b3441ad361a69b434c3b571a3dd8c0e425d27ae1
bofei.zq <bofei.zq@taobao.com>
2017-02-24 15:12:56 +0800

    支持link

---
+++ weex-plugin-demo/ios/weex-plugin-demo/weex-plugin-demo/weex-plugin-demo.h
@@ -0,0 +1,19 @@
+//
+//  weex-plugin-demo.h
+//  weex-plugin-demo
+//
+//  Created by zhangquan on 2017/2/23.
+//  Copyright © 2017年 zhangquan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for weex-plugin-demo.
+FOUNDATION_EXPORT double weex_plugin_demoVersionNumber;
+
+//! Project version string for weex-plugin-demo.
+FOUNDATION_EXPORT const unsigned char weex_plugin_demoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <weex_plugin_demo/PublicHeader.h>
+
+
