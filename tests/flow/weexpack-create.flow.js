a756d30a5d2ada8e88803434349ed871300d4dfb
JackPu <kakashjack@gmail.com>
2017-03-13 20:38:08 +0800

    update ci

--- tests/flow/weexpack-create.flow.js
+++ tests/flow/weexpack-create.flow.js
@@ -3,14 +3,17 @@ const chai = require('chai');
 const expect = chai.expect;
 const fs = require('fs');
 const child_process = require('child_process');
+const path = require('path');
+
 module.exports = function () {
   it('test weexpack create command', function (done) {
     child_process.exec('rm -rf weexpackdemo &&  node ./bin/weexpack-create weexpackdemo');
     setTimeout(function () {
-      expect(fs.existsSync('./weexpackdemo/package.json')).to.equal(true);
-      expect(fs.existsSync('./weexpackdemo/src/index.we')).to.equal(true);
-      expect(fs.existsSync('./weexpackdemo/plugins')).to.equal(true);
-      child_process.exec('cd weexpackdemo && node ../bin/weexpack-build web');
+      const pkg = fs.existsSync(path.join(__dirname, '../../weexpackdemo/package.json'));
+      const srcwe = fs.existsSync(path.join(__dirname, '../../weexpackdemo/src/index.we'));
+      const plugin = fs.existsSync(path.join(__dirname, '../../weexpackdemo/plugins'));
+      expect(pkg && srcwe && plugin).to.equal(true);
+      child_process.exec('cd .jonweexpackdemo && node ../bin/weexpack-build web');
       // child_process.exec('node ../bin/weexpack-platform add ios');
       child_process.exec('cd weexpackdemo && node ../bin/weexpack-platform add android');
       child_process.exec('cd weexpackdemo && node ../bin/weexpack-plugin add weex-plugin-amap');
--- tests/flow/weexpack-plugin-create.flow.js
+++ tests/flow/weexpack-plugin-create.flow.js
@@ -9,10 +9,10 @@ module.exports = function () {
     child_process.exec('rm -rf weex-plugin &&  node ./bin/weexpack-plugin create  weex-plugin');
     setTimeout(function () {
       const hasPackage = fs.existsSync(path.join(__dirname, '../../weex-plugin/package.json'));
-      const hasConfig = fs.existsSync(path.join(__dirname, './weex-plugin/plugin.xml'));
-      const hasWeb = fs.existsSync(path.join(__dirname, './weex-plugin/web'));
-      const hasAndroid = fs.existsSync(path.join(__dirname, './weex-plugin/android'));
-      const hasIos = fs.existsSync(path.join(__dirname, './weex-plugin/ios'));
+      const hasConfig = fs.existsSync(path.join(__dirname, '../../weex-plugin/plugin.xml'));
+      const hasWeb = fs.existsSync(path.join(__dirname, '../../weex-plugin/web'));
+      const hasAndroid = fs.existsSync(path.join(__dirname, '../../weex-plugin/android'));
+      const hasIos = fs.existsSync(path.join(__dirname, '../../weex-plugin/ios'));
       const fileCheck = (hasPackage && hasConfig && hasWeb && hasAndroid && hasIos);
       expect(fileCheck).to.equal(true);
       done();
