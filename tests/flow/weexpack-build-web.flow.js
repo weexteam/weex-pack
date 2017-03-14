a481485ab06ec4a87bdf79aeb3385c4f9e4dbc99
JackPu <kakashjack@gmail.com>
2017-03-14 14:47:27 +0800

    update tests

--- tests/flow/weexpack-build-web.flow.js
+++ tests/flow/weexpack-build-web.flow.js
@@ -7,9 +7,10 @@ const path = require('path');
 module.exports = function () {
   it('test weexpack build web command', function (done) {
     setTimeout(function () {
+      console.log(fs.existsSync(path.join(__dirname, '../../weexpackdemo/dist/index.js')));
       expect(fs.existsSync(path.join(__dirname, '../../weexpackdemo/dist/index.js'))).to.equal(true);
       done();
-    }, 30000);      
+    }, 50000);      
   });
 }
 
