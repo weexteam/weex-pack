5fb1403c04c60d4d64d1326b0751baf11d61e8e8
gaosong <rex.gs@alibaba-inc.com>
2017-03-16 17:55:31 +0800

    0.3.13 release support npm install a plugin and adapter namespace system 支持用npm安装一个插件的js部分,自动处理依赖库的命名空间问题 但是npm安装的这个插件的时候,如果这个插件带命名空间,那么必须npm install 全名

---
+++ src/publish/weexpack-hook.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+var path = require('path');
+deps.forEach(function (d) {
+  var entry;
+  try {
+     entry = require.resolve(d.fullname);
+  }catch(e){}
+  if(entry) {
+    var basePath = entry.substr(0, entry.indexOf(d.fullname) + d.fullname.length);
+    var pkgPath = path.join(basePath, 'package.json');
+    var pkg = JSON.parse(fs.readFileSync(pkgPath));
+    pkg.name = d.name;
+    fs.writeFileSync(pkgPath, JSON.stringify(pkg), null, 4);
+    var newPath = basePath.replace(new RegExp(d.fullname + '$'), d.name);
+    if (!fs.existsSync(newPath)) {
+      fs.symlinkSync(basePath, newPath)
+    }
+  }
+
+});
